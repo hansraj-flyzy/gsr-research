43b86d39745ea2bba41a9620f8ff96a941ee66f4
hansraj-flyzy <hansraj@flyzygo.com>
2022-07-04 03:01:34 +0530

    Updated url

--- src/urls.js
+++ src/urls.js
@@ -2,4 +2,4 @@ exports.apiBaseUrl = 'http://localhost:8080';
 exports.folderId = '1CTJxdA_JWsnAhlFBVQHwfm81Xq_WBku_'
 exports.clientId = '359454221523-i172pd3hjpr3d065biu5ojqahmuvah85.apps.googleusercontent.com'
 exports.apiKey = 'AIzaSyDcQoD06tKbyAOn7PS0PbCKgV0stoPQ2Og'
-exports.appScriptUrl = 'https://script.google.com/macros/s/AKfycbzbh8cjiyKscq738MSXs5KwDus7g7bmrY1CmyTHhsgz2J0yk7hW5jXx6-Ibim_BQrij/exec';
+exports.appScriptUrl = 'https://script.google.com/macros/s/AKfycbz3wFFHpHF5SElKVCsb5SMf8LSUrKLUXaK2LMw6F4FjgxQK4vgSrv2wGVJ4zJj6pH0/exec';
