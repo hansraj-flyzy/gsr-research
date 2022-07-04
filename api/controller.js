55fbe6b670ed30a5a74ed6b578f31916d2869f11
hansraj-flyzy <hansraj@flyzygo.com>
2022-07-04 12:56:21 +0530

    Added full screen buttons etc.

--- api/controller.js
+++ api/controller.js
@@ -1,3 +1,4 @@
+const { default: axios } = require('axios');
 const fs = require('fs');
 const { folderId } = require('../src/urls');
 
@@ -13,5 +14,29 @@ exports.saveRowToFile = async (req, res) => {
     } catch (error) {
         res.send({ success: false, error: error.message })
         console.error(error);
+    }
+}
+
+exports.checkForUpdates=async (req, res)=>{
+    try {
+        let resp = await axios.get('https://us-central1-tusc-91a8b.cloudfunctions.net/gsr/getProdVersion')
+        let data = resp.data()
+        console.log('version', data);
+        if(data.success){
+            // const currentVersion = fs.readFileSync('~/gsrProdVersion',
+            // {encoding:'utf8', flag:'r'});
+            return{
+                success: true,
+                currentVersion: data.version,
+                updatesAvailable: true,
+                latestVersion: data.version
+            }
+        }
+        
+    } catch (error) {
+        res.send({
+            success: false,
+            error: error.message
+        })
     }
 }
--- api/routes.js
+++ api/routes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { saveRowToFile } = require('./controller');
+const { saveRowToFile, checkForUpdates } = require('./controller');
 
 router.post("/addRow", saveRowToFile);
 router.use(express.static("build"));
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+router.post('/checkForUpdates', checkForUpdates)
 
 
 module.exports = router;
