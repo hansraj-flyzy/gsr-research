24b4b04f788ffb0bef56d3a5f1a2c954c4e0836d
hansraj-flyzy <hansraj@flyzygo.com>
2022-07-04 17:47:29 +0530

    Changed ports back to original

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 var cors = require('cors')
 const app = express()
-const port = 9000
+const port = 8080
 app.use(require('body-parser').json()); 
 app.use(cors({
   origin: '*'
